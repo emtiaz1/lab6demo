8a83de3768b930c590426d415da08565826c8041
emtiaz1 <rafid15-5151@diu.edu.bd>
2025-02-26 09:37:02 +0600

    lab6demo

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdvancedUI());
+}
+
+class AdvancedUI extends StatelessWidget {
+  const AdvancedUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
